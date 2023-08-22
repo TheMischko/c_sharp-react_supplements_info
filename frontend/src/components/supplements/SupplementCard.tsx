3ca225f6fe53a348282d8e081fd7fcb39e444fd3
Michal_Dvorak <michal.dvorak999@gmail.com>
2023-08-22 09:12:50 +0200

    Frontend: Added usage of new color mapping for background of cards

--- frontend/src/components/supplements/SupplementCard.tsx
+++ frontend/src/components/supplements/SupplementCard.tsx
@@ -1,14 +1,18 @@
-import {Card, Stack, Text} from "@chakra-ui/react";
+import {Card, Heading, Stack, Text} from "@chakra-ui/react";
 import {Supplement} from "../../model/supplement";
+import { getColorForSupplement } from "../../service/supplementService/supplementCategoryColorMap";
 
 export interface SupplementCardProps {
     supplement: Supplement
 }
 export default function SupplementCard(props: SupplementCardProps) {
+    const bgColor = getColorForSupplement(props.supplement);
+
+
     return(
-      <Card m="2" p="1">
+      <Card m="2" p="1" backgroundColor={bgColor} textColor="white">
           <Stack align="center">
-              <Text>{props.supplement.name}</Text>
+              <Heading size="md">{props.supplement.name}</Heading>
               <Text>Category: {props.supplement.category}</Text>
               <Text>Improves: {props.supplement.claimedImprovement}</Text>
               <Stack direction="row">
